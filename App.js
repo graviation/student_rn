d77f0bc09a3ed4a27951202b7f1fee70e8473ac0
liuy <liuyang_06_01@163.com>
2020-06-26 18:39:56 +0800

    * 导航测试

--- App.js
+++ App.js
@@ -6,29 +6,103 @@
  * @flow strict-local
  */
 
+import 'react-native-gesture-handler';
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import {View, Text, Button, StatusBar} from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {NavigationContainer, useFocusEffect} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import TransparentStatusBar from './app/components/TransparentStatusBar';
 
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+const HomeRoute: () => React$Node = ({route, navigation}) => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name={'Home'} component={HomeScreen} />
+      <Tab.Screen name={'Home1'} component={Home1Screen} />
+      <Tab.Screen name={'Home2'} component={Home2Screen} />
+    </Tab.Navigator>
+  );
+};
+
+const HomeScreen: () => React$Node = ({route, navigation}) => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <TransparentStatusBar />
+      <Text>Home Screen</Text>
+      <Button
+        title={'Go To Home1'}
+        onPress={() => navigation.navigate('Home1')}
+      />
+      <Button
+        title={'Go To Home2'}
+        onPress={() => navigation.navigate('Home2')}
+      />
+    </View>
+  );
+};
+const Home1Screen: () => React$Node = ({route, navigation}) => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <TransparentStatusBar />
+      <Text>Home1 Screen</Text>
+      <Button
+        title={'Go To Home2'}
+        onPress={() => navigation.navigate('Home2')}
+      />
+    </View>
+  );
+};
+const Home2Screen: () => React$Node = ({route, navigation}) => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <TransparentStatusBar />
+      <Text>Home2 Screen</Text>
+      <Button
+        title={'Go To Details'}
+        onPress={() =>
+          navigation.navigate('Details', {
+            itemId: 10086,
+          })
+        }
+      />
+    </View>
+  );
+};
+const DetailsScreen: () => React$Node = ({route, navigation}) => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <StatusBar />
+      <Text>Details Screen</Text>
+      <Button
+        title={'Go To Home2'}
+        onPress={() => navigation.navigate('Home2')}
+      />
+    </View>
+  );
+};
 const App: () => React$Node = () => {
   return (
-    <>
-      <Text>123</Text>
-    </>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name={'HomeRoute'}
+          component={HomeRoute}
+          options={{
+            title: '首页',
+          }}
+        />
+        <Stack.Screen
+          name={'Details'}
+          component={DetailsScreen}
+          options={{
+            title: '详情',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
---
+++ app/components/TransparentStatusBar.js
@@ -0,0 +1,17 @@
+/**
+ * 半透明的状态栏
+ */
+
+import React from 'react';
+import {StatusBar} from 'react-native';
+
+const TransparentStatusBar: () => React$Node = () => {
+  return (
+    <StatusBar
+      barStyle={'light-content'}
+      translucent={true}
+      backgroundColor={'rgba(255,255,255,0)'} //颜色透明
+    />
+  );
+};
+export default TransparentStatusBar;
